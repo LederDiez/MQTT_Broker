aff5ea3f86f6d6cec05de412905177745e6f0ec5
DESKTOP-5JBC0JC\leder <lederdiez00@gmail.com>
2022-05-07 13:47:05 -0500

    Revert "Configuraciones de puertos agregadas"
    This reverts commit 600aba3962dfe8976a013b35acbae108306e9a45.

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-	PORT1 : process.env.PORT 					|| 1883,
-	PORT2 : process.env.PORT 					|| 1884,
+	PORT : process.env.PORT 					|| 1883,
 	NODE_ENV : process.env.NODE_ENV 	|| 'development',
 	MONGO_URL : process.env.MONGO_URL || 'MONGO_URL'
 }
