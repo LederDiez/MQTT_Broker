6bfcad458343bf28d071164ea72418d0bf954f7a
DESKTOP-5JBC0JC\leder <lederdiez00@gmail.com>
2022-08-27 16:49:19 -0500

    Custom ports not supported.

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ dotenv.config()
 const configs = {
   USER_COLECTION: process.env.USER_COLECTION || 'USER_COLECTION',
   DEVICE_COLECTION: process.env.DEVICE_COLECTION || 'DEVICE_COLECTION',
-  PORT: process.env.SOCKET_PORT || 80,
+  PORT: process.env.PORT || 80,
   NODE_ENV: process.env.NODE_ENV || 'development',
   MONGO: process.env.MONGO || 'MONGO_URL'
 }
